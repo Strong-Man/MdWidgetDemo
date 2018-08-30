b9db5b5c444ee1b259baea898df8bc41677e3741
Strong-Man <130240770@qq.com>
2018-08-30 11:42:50 +0800

    添加:toolbar

--- app/src/main/java/com/example/administrator/mdwidgetdemo/ToolbarAct.java
+++ app/src/main/java/com/example/administrator/mdwidgetdemo/ToolbarAct.java
@@ -3,6 +3,14 @@ package com.example.administrator.mdwidgetdemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by zpw on 2018/8/15.
@@ -13,11 +21,41 @@ import android.support.annotation.Nullable;
  * 操作菜单
  */
 
-public class ToolbarAct extends Activity {
+public class ToolbarAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
+        initView();
+
+    }
+
+    private void  initView(){
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar,menu);
+        return true;
+    }
+
+    @Override
+    protected boolean onPrepareOptionsPanel(View view, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass() == MenuBuilder.class) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        return super.onPrepareOptionsPanel(view, menu);
     }
 }
